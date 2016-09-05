e9491d38089f28c7bf9be0c009b97e0674b7a12b
trikitrok <trikitrok@gmail.com>
2016-09-05 14:28:05 +0200

    add lein auto to run Clojure tests every time a file changes

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
                  [org.clojure/clojurescript "1.9.227"]]
 
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-doo "0.1.6"]]
+            [lein-doo "0.1.6"]
+            [lein-auto "0.1.2"]]
+
+  :auto {"test" {:file-pattern #"\.(clj|cljs|cljc|edn)$"}}
 
   :clean-targets ^{:protect false} ["resources/public/js" "target" "out"]
 
